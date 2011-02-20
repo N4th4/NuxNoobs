6d0ea5824d18104cfa61c3c459d6991b3baac806
Nathanaël Restori <nathidem@hotmail.com>
2011-02-20 09:28:18 +0100

    Move to the shorter constructor

--- src/com/bukkit/N4th4/NuxNoob/NuxNoob.java
+++ src/com/bukkit/N4th4/NuxNoob/NuxNoob.java
@@ -1,14 +1,11 @@
 package com.bukkit.N4th4.NuxNoob;
 
-import java.io.File;
 import java.util.HashMap;
 
 import org.bukkit.entity.Player;
-import org.bukkit.Server;
 import org.bukkit.event.Event.Priority;
 import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.PluginLoader;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.PluginManager;
 
@@ -16,8 +13,7 @@ public class NuxNoob extends JavaPlugin {
     private final NuxNoobPlayerListener playerListener;
     private final HashMap<Player, Boolean> debugees;
 
-    public NuxNoob(PluginLoader pluginLoader, Server instance, PluginDescriptionFile desc, File folder, File plugin, ClassLoader cLoader) {
-        super(pluginLoader, instance, desc, folder, plugin, cLoader);
+    public NuxNoob() {
         NuxNoobLogger.initialize();
         playerListener = new NuxNoobPlayerListener(this);
         debugees = new HashMap<Player, Boolean>();
